15eba9c3e74a419d1c8bb593c0821702d75832c3
Dani <dani.d.zamorano@gmail.com>
2019-03-14 17:50:41 +0100

    setup jwt authentication strategy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -17,7 +18,11 @@ app.use(bodyParser.json());
 
 mongoose.connect(db).then(() => console.log('MongoDB Connected')).catch((err) => console.log(err));
 
-app.get('/', (req, res) => res.send('TEST!!'));
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
 
 // Use routes
 app.use('/api/users', users);
