16636697cc11deb8950159926a2969b4b3bc3564
Dani <dani.d.zamorano@gmail.com>
2019-03-21 00:29:18 +0100

    add current profile router

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route   GET api/profile/test
-router.get('/test', (req, res) => res.json({ msg: 'profile work' }));
+router.get('/test', (req, res) => res.json({ msg: 'profile works' }));
+
+// @route   GET api/profile
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+	const errors = {};
+	Profile.findOne({ user: req.user.id })
+		.then((profile) => {
+			if (!profile) {
+				errors.noprofile = 'There is no profile for this user';
+				return res.status(404).json(errors);
+			}
+			res.json(profile);
+		})
+		.catch((err) => res.status(404).json(err));
+});
+
+// @route   GET api/profile/test
 
 module.exports = router;
