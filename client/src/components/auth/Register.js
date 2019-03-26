0e3ab5dcf840f79ec2f7af9532bd9dd1ee0280d9
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-03-26 18:12:18 +0100

    add register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
-	return <div>Register</div>;
+	const [ name, setName ] = useState('');
+	const [ email, setEmail ] = useState('');
+	const [ password, setPassword ] = useState('');
+	const [ password2, setPassword2 ] = useState('');
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		const newUser = {
+			name,
+			email,
+			password,
+			password2
+		};
+
+		console.log(newUser);
+	};
+
+	return (
+		<div className="register">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-8 m-auto">
+						<h1 className="display-4 text-center">Sign Up</h1>
+						<p className="lead text-center">Create your DevConnector account</p>
+						<form action="create-profile.html" onSubmit={onSubmit}>
+							<div className="form-group">
+								<input
+									type="text"
+									className="form-control form-control-lg"
+									placeholder="Name"
+									name="name"
+									value={name}
+									onChange={(e) => setName(e.target.value)}
+									required
+								/>
+							</div>
+							<div className="form-group">
+								<input
+									type="email"
+									className="form-control form-control-lg"
+									placeholder="Email Address"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									name="email"
+								/>
+								<small className="form-text text-muted">
+									This site uses Gravatar so if you want a profile image, use a Gravatar email
+								</small>
+							</div>
+							<div className="form-group">
+								<input
+									type="password"
+									className="form-control form-control-lg"
+									placeholder="Password"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									name="password"
+								/>
+							</div>
+							<div className="form-group">
+								<input
+									type="password"
+									className="form-control form-control-lg"
+									placeholder="Confirm Password"
+									value={password2}
+									onChange={(e) => setPassword2(e.target.value)}
+									name="password2"
+								/>
+							</div>
+							<input type="submit" className="btn btn-info btn-block mt-4" />
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
