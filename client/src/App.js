8c10badb6f937c50797a14a8151601d4b1baaab4
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-03-26 16:01:34 +0100

    install react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Footer from './components/layout/Footer';
 import './styles/App.css';
 
-class App extends Component {
-	render() {
-		return (
+const App = () => {
+	return (
+		<Router>
 			<div className="App">
 				<Navbar />
-				<Landing />
+				<Route exact path={'/'} component={Landing} />
 				<Footer />
 			</div>
-		);
-	}
-}
+		</Router>
+	);
+};
 
 export default App;
