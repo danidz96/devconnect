640953d98a6a0e1a5ed7b82d04138fe1ac9dbd85
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-03-28 20:17:34 +0100

    refactor hooks

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,18 +1,17 @@
 import React, { useState } from 'react';
 
 const Register = () => {
-	const [ name, setName ] = useState('');
-	const [ email, setEmail ] = useState('');
-	const [ password, setPassword ] = useState('');
-	const [ password2, setPassword2 ] = useState('');
+	const [ values, setValues ] = useState({ name: '', email: '', password: '', password2: '' });
+
+	const onChange = (input) => {
+		setValues({ ...values, [input.name]: input.value });
+	};
+
 	const onSubmit = (e) => {
 		e.preventDefault();
 
 		const newUser = {
-			name,
-			email,
-			password,
-			password2
+			...values
 		};
 
 		console.log(newUser);
@@ -32,8 +31,8 @@ const Register = () => {
 									className="form-control form-control-lg"
 									placeholder="Name"
 									name="name"
-									value={name}
-									onChange={(e) => setName(e.target.value)}
+									value={values.name}
+									onChange={(e) => onChange(e.target)}
 									required
 								/>
 							</div>
@@ -42,8 +41,8 @@ const Register = () => {
 									type="email"
 									className="form-control form-control-lg"
 									placeholder="Email Address"
-									value={email}
-									onChange={(e) => setEmail(e.target.value)}
+									value={values.email}
+									onChange={(e) => onChange(e.target)}
 									name="email"
 								/>
 								<small className="form-text text-muted">
@@ -55,8 +54,8 @@ const Register = () => {
 									type="password"
 									className="form-control form-control-lg"
 									placeholder="Password"
-									value={password}
-									onChange={(e) => setPassword(e.target.value)}
+									value={values.password}
+									onChange={(e) => onChange(e.target)}
 									name="password"
 								/>
 							</div>
@@ -65,8 +64,8 @@ const Register = () => {
 									type="password"
 									className="form-control form-control-lg"
 									placeholder="Confirm Password"
-									value={password2}
-									onChange={(e) => setPassword2(e.target.value)}
+									value={values.password2}
+									onChange={(e) => onChange(e.target)}
 									name="password2"
 								/>
 							</div>
