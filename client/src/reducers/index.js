fcf59e6364db8e76024d759e8edd58192908b126
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-08 16:12:13 +0200

    more setup

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 
-export default combineReducers();
+export default combineReducers({
+	auth: {
+		authReducer
+	}
+});
