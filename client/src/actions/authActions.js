5fd43f0acfd362ba90d2c4763bfffa333138c4d5
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-09 19:31:48 +0200

    add login action and auth reducer

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from '../utils/setAuthToken';
 
+// Register User
 export const registerUser = (userData) => (dispatch) => {
 	axios.post('/api/users/register', userData).then((res) => console.log(res.data)).catch((err) =>
 		dispatch({
@@ -7,4 +10,36 @@ export const registerUser = (userData) => (dispatch) => {
 			payload: err.response.data
 		})
 	);
+};
+
+// Login - Get User Token
+export const loginUser = (userData) => (dispatch) => {
+	axios
+		.post('/api/users/login', userData)
+		.then((res) => {
+			// Save to localStorage
+			const { token } = res.data;
+			// Set token to ls
+			localStorage.setItem('jwtToken', token);
+			// Set token to Auth Header
+			setAuthToken(token);
+			// Decode token to get user data
+			const decoded = jwt_decode(token);
+			// Set current user
+			dispatch(setCurrentUser(decoded));
+		})
+		.catch((err) =>
+			dispatch({
+				type: 'GET_ERRORS',
+				payload: err.response.data
+			})
+		);
+};
+
+// Set logged in user
+export const setCurrentUser = (decoded) => {
+	return {
+		type: 'SET_CURRENT_USER',
+		payload: decoded
+	};
 };
