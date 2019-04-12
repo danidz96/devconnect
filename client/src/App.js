3e34d99273d809f2b3f6b32ded3a8600afda9e49
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-04-12 20:01:21 +0200

    setup private routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
@@ -11,6 +11,7 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import './styles/App.css';
+import PrivateRoute from './components/common/PrivateRoute';
 import store from './store';
 
 // Check for token
@@ -43,7 +44,9 @@ const App = () => {
 					<div className="container">
 						<Route exact path={'/register'} component={Register} />
 						<Route exact path={'/login'} component={Login} />
-						<Route exact path={'/dashboard'} component={Dashboard} />
+						<Switch>
+							<PrivateRoute exact path="/dashboard" component={Dashboard} />
+						</Switch>
 					</div>
 					<Footer />
 				</div>
