048e92ffcddea180892c9da7b2b2739df8cf6b5c
Dani <dani.d.zamorano@gmail.com>
2019-04-17 11:47:53 +0200

    create textarea component

---
+++ client/src/components/common/TextAreaFieldGroup.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TextAreaFieldGroup = ({ name, placeholder, value, error, info, onChange }) => {
+	return (
+		<div className="form-group">
+			<textarea
+				className={'form-control form-control-lg ' + (error && 'is-invalid')}
+				placeholder={placeholder}
+				name={name}
+				value={value}
+				onChange={onChange}
+			/>
+			{info && <small className="form-text text-muted">{info}</small>}
+			{error && <div className="invalid-feedback">{error}</div>}
+		</div>
+	);
+};
+
+export default TextAreaFieldGroup;
