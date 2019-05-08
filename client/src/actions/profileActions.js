0e41fd325fa6e8386da64db523fce72a45225573
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-05-08 19:08:39 +0200

    add create education action

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -53,6 +53,16 @@ export const addExperience = (experienceData, history) => (dispatch) => {
 	);
 };
 
+// Add education
+export const addEducation = (educationData, history) => (dispatch) => {
+	axios.post('/api/profile/education', educationData).then((res) => history.push('/dashboard')).catch((err) =>
+		dispatch({
+			type: 'GET_ERRORS',
+			payload: err.response.data
+		})
+	);
+};
+
 // Delete account and profile
 export const deleteAccount = () => (dispatch) => {
 	if (window.confirm('Are you sure? This can NOT be undone!')) {
