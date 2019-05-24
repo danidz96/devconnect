bd8bd7cc342f951ca36909f7723ee23861728fe1
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-05-24 13:41:03 +0200

    create Profiles, ProfileItem components

---
+++ client/src/components/profiles/Profiles.js
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import Spinner from '../common/Spinner';
+import { getProfiles } from '../../actions/profileActions';
+
+const Profiles = (props) => {
+	useEffect(() => {
+		props.getProfiles();
+	}, []);
+
+	const { profiles, loading } = props.profile;
+	let profileItems;
+
+	if (profiles === null || loading) {
+		profileItems = <Spinner />;
+	} else {
+		if (profiles.length > 0) {
+			profileItems = <h1>PROFILES</h1>;
+		} else {
+			profileItems = <h4>No profiles found...</h4>;
+		}
+	}
+
+	return (
+		<div className="profiles">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">
+						<h1 className="display-4 text-center">Developer Profiles</h1>
+						<p className="lead text-center">Browse and connect with developers</p>
+						{profileItems}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const mapStateToProps = (state) => ({
+	profile: state.profile
+});
+
+export default connect(mapStateToProps, { getProfiles })(Profiles);
