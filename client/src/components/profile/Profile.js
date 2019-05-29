f2608e82176d22f6e96b2046c5281930bd4f955c
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-05-29 12:42:58 +0200

    add profile header

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Spinner from '../common/Spinner';
 import { getProfileByHandle } from '../../actions/profileActions';
+import ProfileHeader from './ProfileHeader';
 
 const Profile = (props) => {
 	useEffect(() => {
@@ -10,7 +12,36 @@ const Profile = (props) => {
 		}
 	}, []);
 
-	return <div>Profile</div>;
+	const { profile, loading } = props;
+	let profileContent;
+
+	if (profile.profile === null || loading) {
+		profileContent = <Spinner />;
+	} else {
+		profileContent = (
+			<div>
+				<div className="row">
+					<div className="col-md-6">
+						<Link to="/profiles" className="btn btn-light mb-3 float-left">
+							Back To Profiles
+						</Link>
+					</div>
+					<div className="col-md-6" />
+				</div>
+				<ProfileHeader profile={profile} />
+			</div>
+		);
+	}
+
+	return (
+		<div className="profile">
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12">{profileContent}</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 const mapStateToProps = (state) => ({
