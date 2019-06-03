5ea1fcabc2602f05eab5ffd31d40d9c9363ecc35
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-03 19:49:56 +0200

    add delete post comment

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -52,6 +52,23 @@ export const removeLike = (id) => (dispatch) => {
 		.catch((err) => dispatch({ type: 'GET_ERRORS', payload: err.response.data }));
 };
 
+export const deleteComment = (postId, commentId) => (dispatch) => {
+	axios
+		.delete(`/api/posts/comment/${postId}/${commentId}`)
+		.then((res) =>
+			dispatch({
+				type: 'GET_POST',
+				payload: res.data
+			})
+		)
+		.catch((err) =>
+			dispatch({
+				type: 'GET_ERRORS',
+				payload: err.response.data
+			})
+		);
+};
+
 export const setPostLoading = () => {
 	return {
 		type: 'POST_LOADING'
