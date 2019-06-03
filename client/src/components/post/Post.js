63a65e523ffc85dbbaa7ec40d91bf5c876dff90c
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-03 19:24:32 +0200

    show single post

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getPost } from '../../actions/postActions';
+import Spinner from '../common/Spinner';
+import PostItem from '../posts/PostItem';
 
-const Post = () => {
+const Post = (props) => {
+	const { post, loading } = props.post;
+
+	useEffect(() => {
+		props.getPost(props.match.params.id);
+	}, []);
+
+	let postContent;
+
+	if (post === null || loading || Object.keys(post).length === 0) {
+		postContent = <Spinner />;
+	} else {
+		postContent = (
+			<React.Fragment>
+				<PostItem post={post} />
+			</React.Fragment>
+		);
+	}
+
 	return (
 		<div>
-			<h1>Post</h1>
+			<div className="post">
+				<div className="container">
+					<div className="row">
+						<div className="col-md-12">
+							<Link to="/feed" className="btn btn-light mb-3">
+								Back To Feed
+							</Link>
+							{postContent}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default Post;
+const mapStateToProps = (state) => ({
+	post: state.post
+});
+
+export default connect(mapStateToProps, { getPost })(Post);
