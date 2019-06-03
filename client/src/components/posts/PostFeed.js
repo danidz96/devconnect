9544d2a9ed622d83f4a92e86cca98d17983e2641
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-03 15:49:16 +0200

    create PostFeed

---
+++ client/src/components/posts/PostFeed.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PostItem from './PostItem';
+
+const PostFeed = (props) => {
+	const { posts } = props;
+
+	return posts.map((post) => <PostItem key={post._id} post={post} />);
+};
+
+export default PostFeed;
--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PostForm from './PostForm';
 import Spinner from '../common/Spinner';
+import { getPosts } from '../../actions/postActions';
+import PostFeed from './PostFeed';
 
-const Posts = () => {
+const Posts = (props) => {
+	useEffect(() => {
+		props.getPosts();
+	}, []);
+
+	const { posts, loading } = props.post;
+	let postContent;
+
+	if (posts === null || loading) {
+		postContent = <Spinner />;
+	} else {
+		postContent = <PostFeed posts={posts} />;
+	}
+
 	return (
 		<div className="feed">
 			<div className="container">
 				<div className="row">
 					<div className="col-md-12">
 						<PostForm />
+						{postContent}
 					</div>
 				</div>
 			</div>
@@ -21,4 +37,4 @@ const mapStateToProps = (state) => ({
 	post: state.post
 });
 
-export default connect(mapStateToProps)(Posts);
+export default connect(mapStateToProps, { getPosts })(Posts);
