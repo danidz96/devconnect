a16cf7cbcb0318095d9c78dc53850f7425a27b27
Dani Domínguez <dani.d.zamorano@gmail.com>
2019-06-03 19:33:09 +0200

    add comment to post

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -38,6 +38,13 @@ export const addLike = (id) => (dispatch) => {
 		.catch((err) => dispatch({ type: 'GET_ERRORS', payload: err.response.data }));
 };
 
+export const addComment = (postId, commentData) => (dispatch) => {
+	axios
+		.post(`/api/posts/comment/${postId}`, commentData)
+		.then((res) => dispatch({ type: 'GET_POST', payload: res.data }))
+		.catch((err) => dispatch({ type: 'GET_ERRORS', payload: err.response.data }));
+};
+
 export const removeLike = (id) => (dispatch) => {
 	axios
 		.post(`/api/posts/unlike/${id}`)
